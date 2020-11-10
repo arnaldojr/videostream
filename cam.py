3710b56583dac22787d22927831f0288e4586337
Arnaldo Viana <arnaldo_viana@yahoo.com.br>
2020-11-10 00:01:17 -0300

    video stream

--- cam.py
+++ cam.py
@@ -1,28 +1,37 @@
+#!/usr/bin/env python
+
+# exemplos simples de como rodar stream de uma camera IP 
+
 import cv2
 import os
 
-
+#parametros de acesso da camera
 USERNAME = 'admin'
 PASSWORD = '147258Jr'
 IP = '192.168.15.19'
 PORT = '554'
 
+#so roda se for ffmpeg
 os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
 
-
+#url da camera stream varia conforme o modelo da camera
 URL = 'rtsp://{}:{}@{}:{}/onvif1'.format(USERNAME, PASSWORD, IP, PORT)
-print('Connecting to: ' + URL)
+print('Conectando com: ' + URL)
 
-vcap = cv2.VideoCapture(URL, cv2.CAP_FFMPEG)
+cap = cv2.VideoCapture(URL, cv2.CAP_FFMPEG)
 
-while(1):
-    ret, frame = vcap.read()
+while True:
+    ret, frame = cap.read()
     if ret == False:
-        print("Frame is empty")
+        print("Sem frame")
         break
     else:
+
+
         cv2.imshow('VIDEO', frame)
-    cv2.waitKey(1)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-
-# cam = cv2.VideoCapture(URL, cv2.CAP_FFMPEG)
+cap.release()
+cv2.destroyAllWindows()
--- camera.py
+++ camera.py
@@ -1,42 +1,93 @@
-#!/usr/bin/env python
-
-#
-# rtsp links: http://wiki.multimedia.cx/index.php?title=RTSP
-#
-
 import cv2
+import time
+import datetime
+import imutils
 import os
 
-os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
 
-stream = 'rtsp://admin:147258Jr@192.168.15.19/onvif1'
-#stream = 0
+def camera_config():
+    #parametros de acesso da camera
+    USERNAME = 'admin'
+    PASSWORD = '147258Jr'
+    IP = '192.168.15.19'
+    PORT = '554'
 
-cap = cv2.VideoCapture(stream)
+    #so roda se for ffmpeg
+    os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
 
-# check if opened 
-if not cap.isOpened():
-    print("Can't open stream/file")
-else:    
+    #url da camera stream varia conforme o modelo da camera
+    URL = 'rtsp://{}:{}@{}:{}/onvif1'.format(USERNAME, PASSWORD, IP, PORT)
+    print('Conectando com: ' + URL)
+    return URL
+
+#salva um frame de referencia e compara com o atual
+def motion_detection(URL):
+    print('starting...')
+    video_capture = cv2.VideoCapture(URL, cv2.CAP_FFMPEG)
+    #video_capture = cv2.VideoCapture(0) # value (0) selects the devices default camera
+    #time.sleep(2)
+
+    first_frame = None
+
     while True:
-        # read one frame (and "return" status)
-        ret, frame = cap.read()
 
-        # exit if error (check it because it doesn't rise error when it has problem to read)
-        if not ret:
+        ret,frame = video_capture.read()
+        
+        if ret: exit
+        
+        text = 'Normal'
+
+        greyscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+        gaussian_frame = cv2.GaussianBlur(greyscale_frame, (21,21),0)
+        blur_frame = cv2.blur(gaussian_frame, (5,5))
+        greyscale_image = blur_frame
+     
+        if first_frame is None:
+            first_frame = greyscale_image
+        else:
+            pass
+
+
+#        frame = imutils.resize(frame, width=500)
+        frame_delta = cv2.absdiff(first_frame, greyscale_image)
+        thresh = cv2.threshold(frame_delta, 100, 255, cv2.THRESH_BINARY)[1]
+        dilate_image = cv2.dilate(thresh, None, iterations=2)
+        _,cnt,hierarchy = cv2.findContours(dilate_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+
+        for c in cnt:
+            if c is None: print(c, 'nothing!!!')
+            if cv2.contourArea(c) > 800:
+                (x, y, w, h) = cv2.boundingRect(c)
+
+                cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+
+                text = 'Movimento detectado'
+                
+            else:
+                pass
+
+
+        
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        cv2.putText(frame, '{+} Status: %s' % (text),
+            (10,frame.shape[0] - 30), cv2.FONT_HERSHEY_SIMPLEX , 0.5, (0, 0, 255), 2)
+        
+        cv2.putText(frame, datetime.datetime.now().strftime('%A %d %B %Y %I:%M:%S%p'),
+            (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX , 0.35, (0, 0, 255),1)
+       
+        cv2.imshow('Security Feed', frame)
+        #cv2.imshow('Threshold(foreground mask)', dilate_image)
+        #cv2.imshow('Frame_delta', frame_delta)
+
+        key = cv2.waitKey(1) & 0xFF 
+        if key == ord('q'):
+            cv2.destroyAllWindows()
             break
 
-        # (open window and) display one frame
-        cv2.imshow('frame', frame)
 
-        # exit if pressed any key
-        # (it doesn't wait for key so you can read next frame)
-        # (you need opened window to catch pressed key)
-        if cv2.waitKey(1) != -1:
-            break
 
-# close stream
-cap.release()
-
-# close window
-cv2.destroyAllWindows()
+if __name__=='__main__':
+    print('will start now')
+    cam = camera_config()
+    motion_detection(cam)
